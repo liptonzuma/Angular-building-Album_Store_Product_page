7b7f40db1a73004ea97a7311e5691e5c60c63e6d
Lipton Zuma <solomonboateng231@gmail.com>
2020-10-05 03:05:05 +0000

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductService } from './product.service'
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent,
+    ProductService
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
